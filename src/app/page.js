82e367a95af237fcc09acca27c2fee43a14a2aa8
FOLLOO <nafis.safiullin2004@mail.ru>
2024-08-19 14:14:29 +0300

    main page

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,22 @@
-import CaseItem from "@/components/case_item/case_item";
-import ArticleItem from "@/components/article_item/article_item";
+'use client'
 
-export default function Home() {
+// import Image from "next/image";
+import styles from './page.module.css'
+import { useContext } from 'react'
+import { ThemeContext } from '@/contexts/ThemeContext'
+import Organization from '@/components/main/Organization'
+
+
+export default function Home () {
+
+  const { toggle } = useContext(ThemeContext)
+
   return (
-    <div>
-      <CaseItem/>
-      <ArticleItem/>
-    </div>
-  );
+    <main className={styles.main}>
+      <button onClick={() => toggle()}>
+        Change Theme
+      </button>
+        <Organization/>
+    </main>
+  )
 }
