e6d943640cb4326eda0537b3eb9b7c6e86ad9319
FOLLOO <nafis.safiullin2004@list.ru>
2024-08-29 16:25:57 +0300

    alfa

--- src/components/layout/bottom-menu/BottomMenu.jsx
+++ src/components/layout/bottom-menu/BottomMenu.jsx
@@ -1,12 +1,18 @@
 'use client'
-import React from 'react';
+import React, {useContext} from 'react';
 
 import './bottom-menu.css'
+import {useRouter} from "next/navigation";
+import {ThemeContext} from "@/contexts/ThemeContext";
 
 export default function BottomMenu() {
+
+    const router = useRouter()
+    const { toggle } = useContext(ThemeContext)
+
     return (
         <section className="section">
-            <label title="home" htmlFor="home" className="label">
+            <label title="home" htmlFor="home" className="label" onClick={() => router.push('/')}>
                 <input id="home" name="page" type="radio"/>
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
@@ -21,39 +27,58 @@ export default function BottomMenu() {
                     ></path>
                 </svg>
             </label>
-            <label title="favorite" htmlFor="favorite" className="label">
+            <label title="favorite" htmlFor="favorite" className="label" onClick={() => router.push('/posts')}>
                 <input id="favorite" name="page" type="radio"/>
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="inherit"
                     // filter="black"
-                    viewBox="0 0 24 24"
-                    height="20"
+                    viewBox="0 0 24 20"
+                    height="24"
                     width="24"
                     className="icon book"
                 >
-                    <rect x="2" y="0" width="24" height="24" rx="3" stroke="var(--nuans-color)"/>
-                    <path d="M5 10L24 10" stroke="var(--nuans-color)" strokeLinecap="round"/>
+                    <path fillRule="evenodd"
+                          clipRule="evenodd"
+                          d="M4.00174 9H19.9983C19.9862 6.82497 19.8897 5.64706 19.1213 4.87868C18.2426 4 16.8284 4 14 4H10C7.17157 4 5.75736 4 4.87868 4.87868C4.1103 5.64706 4.01384 6.82497 4.00174 9ZM20 11H4V14C4 16.8284 4 18.2426 4.87868 19.1213C5.75736 20 7.17157 20 10 20H14C16.8284 20 18.2426 20 19.1213 19.1213C20 18.2426 20 16.8284 20 14V11Z"
+                          fill="var(--col-darkGray)"
+                            id="col"
+                    />
+
+                    {/*<rect x="0" y="0" width="24" height="24" rx="6" stroke="transparent"/>*/}
+                    {/*<path d="M1 10L23 10" stroke="var(--base-color)" strokeLinecap="round"/>*/}
                 </svg>
             </label>
-            <label title="notifications" htmlFor="notifications" className="label">
+            <label title="notifications" htmlFor="notifications" className="label" onClick={() => router.push('/cases')}>
                 <input id="notifications" name="page" type="radio"/>
                 <svg
                     xmlns="http://www.w3.org/2000/svg"
                     fill="inherit"
-                    viewBox="0 0 16 20"
-                    height="20"
-                    width="16"
+                    viewBox="0 0 22 24"
+                    height="28"
+                    width="28"
                     className="icon history"
                 >
-                    <path
-                        fill="inherit"
-                        d="M15.0294 12.4902L14.0294 10.8302C13.8194 10.4602 13.6294 9.76016 13.6294 9.35016V6.82016C13.6282 5.70419 13.3111 4.61137 12.7147 3.66813C12.1183 2.72489 11.267 1.96978 10.2594 1.49016C10.0022 1.0335 9.62709 0.654303 9.17324 0.392195C8.71939 0.130087 8.20347 -0.00530784 7.6794 0.000159243C6.5894 0.000159243 5.6094 0.590159 5.0894 1.52016C3.1394 2.49016 1.7894 4.50016 1.7894 6.82016V9.35016C1.7894 9.76016 1.5994 10.4602 1.3894 10.8202L0.379396 12.4902C-0.0206039 13.1602 -0.110604 13.9002 0.139396 14.5802C0.379396 15.2502 0.949396 15.7702 1.6894 16.0202C3.6294 16.6802 5.6694 17.0002 7.7094 17.0002C9.7494 17.0002 11.7894 16.6802 13.7294 16.0302C14.4294 15.8002 14.9694 15.2702 15.2294 14.5802C15.4894 13.8902 15.4194 13.1302 15.0294 12.4902ZM10.5194 18.0102C10.3091 18.5923 9.92467 19.0956 9.41835 19.4516C8.91203 19.8077 8.30837 19.9992 7.6894 20.0002C6.8994 20.0002 6.1194 19.6802 5.5694 19.1102C5.2494 18.8102 5.0094 18.4102 4.8694 18.0002C4.9994 18.0202 5.1294 18.0302 5.2694 18.0502C5.4994 18.0802 5.7394 18.1102 5.9794 18.1302C6.5494 18.1802 7.1294 18.2102 7.7094 18.2102C8.2794 18.2102 8.8494 18.1802 9.4094 18.1302C9.6194 18.1102 9.8294 18.1002 10.0294 18.0702L10.5194 18.0102Z"
-                    ></path>
+                    <path d="M22 10.32C22 9.25914 21.5786 8.24172 20.8284 7.49158C20.0783 6.74143 19.0609 6.32001 18 6.32001H6C4.93913 6.32001 3.92178 6.74143 3.17163 7.49158C2.42149 8.24172 2 9.25914 2 10.32"
+                          fill="var(--col-darkGray)"
+                          id='col'
+                          stroke="inherit" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                    <path d="M22 10.32C15.91 16.32 8.09 16.32 2 10.32"
+                          fill="var(--col-darkGray)"
+                          id='col'
+                          stroke="inherit" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                    <path d="M21.9999 10.32L20.9999 18.32C20.8507 19.4088 20.3192 20.4089 19.5002 21.1417C18.6813 21.8745 17.6285 22.2922 16.5299 22.32H7.38989C6.29132 22.2922 5.23847 21.8745 4.41956 21.1417C3.60064 20.4089 3.0691 19.4088 2.91992 18.32L1.91992 10.32"
+                          fill="var(--col-darkGray)"
+                          id='col'
+                          stroke="inherit" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+                    <path d="M8.00977 6.03998C8.00977 4.97911 8.43119 3.96169 9.18134 3.21155C9.93148 2.4614 10.9489 2.03998 12.0098 2.03998C13.0706 2.03998 14.0881 2.4614 14.8382 3.21155C15.5883 3.96169 16.0098 4.97911 16.0098 6.03998"
+                          fill="transparent"
+                          id='col2'
+                          stroke="var(--col-darkGray)" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
                 </svg>
             </label>
-            <label title="cart" htmlFor="cart" className="label">
-                <input id="cart" name="page" type="radio"/>
+            <div title="cart"  className="label" onClick={() => toggle()} >
+                {/*<input id="cart" name="page" type="radio"/>*/}
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="inherit" xmlns="http://www.w3.org/2000/svg"
                      className="icon theme">
                     <path
@@ -61,7 +86,7 @@ export default function BottomMenu() {
                         fill="inherit">
                     </path>
                 </svg>
-            </label>
+            </div>
         </section>
     );
 };
--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -14,7 +14,20 @@ export default function Header () {
   const { theme, toggle } = useContext(ThemeContext)
   const  pathname  = usePathname();
 
-
+  const Imaru = () => {
+      return(
+          <div title="cart"  className="label"  >
+              {/*<input id="cart" name="page" type="radio"/>*/}
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="inherit" xmlns="http://www.w3.org/2000/svg"
+                   className="icon theme">
+                  <path
+                      d="M12,24 C18.6274547,24 24,18.6274547 24,12 C24,5.37254525 18.6274547,0 12,0 C5.37254525,0 0,5.37254525 0,12 C0,18.6274547 5.37254525,24 12,24 Z M12,22 L12,2 C16.9706013,2 21,6.02939872 21,12 C21,17.9706013 16.9706013,22 12,22 Z"
+                      fill="var(--base-color)">
+                  </path>
+              </svg>
+          </div>
+      )
+    }
 
   return (
     <nav className={styles.header}>
@@ -29,7 +42,9 @@ export default function Header () {
           <TransprentButton text={'Блоги'} active={pathname === '/posts'} click={() => router.push('/posts')}/>
           <TransprentButton text={'О нас'} active={pathname === '/'} click={() => router.push('/')} />
           <TransprentButton text={'Кейсы'} active={pathname === '/cases'} click={() => router.push('/cases')}/>
-          <TransprentButton text={'Сменить тему'} click={() => toggle()}/>
+          <TransprentButton
+              img={<Imaru/>}
+              click={() => toggle()}/>
         </nav>
       </section>
     </nav>
