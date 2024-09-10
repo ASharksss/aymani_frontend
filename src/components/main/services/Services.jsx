40a9733459f68f88012ffdb99fc9452b953f01a9
FOLLOO <nafis.safiullin2004@list.ru>
2024-09-10 12:08:45 +0300

    temp 12 services

--- src/components/main/services/Services.jsx
+++ src/components/main/services/Services.jsx
@@ -31,7 +31,7 @@ export default function Services () {
       </div>
         <div className={styles.content} key={'content'}>
             {services?.items?.length > 0 ?
-                services?.items.slice(0,6).map((item, index) => (
+                services?.items.slice(0,12).map((item, index) => (
                     <div className={`${page[`grid${(index % 14) + 1}`]}`} key={item.id}>
                         <CaseItem key={item.id} name={item.name} image={item?.image_url}  price={item.price} click={false}/>
                     </div>
@@ -41,7 +41,7 @@ export default function Services () {
         {/*КОСТЫЛЬ из-за positions */}
         <div className={styles.content2} key={'content2'}>
             {services?.items?.length > 0 ?
-                services?.items.slice(0,6).map((item, index) => (
+                services?.items.slice(0,12).map((item, index) => (
                     <div className={`${page[`grid${(index % 14) + 1}`]}`} key={item.id}>
                         <CaseItem name={item.name} key={item.id} image={item?.image_url}  price={item.price} />
                     </div>
